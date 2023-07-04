8994cb859c9af8d2cb502dbeb516f04ad6a5fb1e
kamilksi <kamilksiazek002@gmail.com>
2023-07-04 18:43:10 +0200

    finished home view

--- lib/presentation/home/view/home_view.dart
+++ lib/presentation/home/view/home_view.dart
@@ -1,49 +1,94 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:logger/logger.dart';
-import 'package:todo_riverpod/model/task_model.dart';
+
 import 'package:todo_riverpod/presentation/add/view/add_view.dart';
+import 'package:todo_riverpod/presentation/edit/view/edit_view.dart';
 import 'package:todo_riverpod/presentation/home/viewmodel/home_model.dart';
 
 class HomeView extends HookConsumerWidget {
-  HomeView({super.key});
+  const HomeView({super.key});
 
-  var logger = Logger();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final asyncTodos = ref.watch(todoProvider);
+    final notifierTodos = ref.read(todoProvider.notifier);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo"),
+        title: const Text("Todo"),
         centerTitle: true,
         backgroundColor: Colors.indigo[300],
       ),
       body: asyncTodos.when(
         data: (todos) => ListView.builder(
           itemCount: todos.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(
-              todos[index].title,
-              style: TextStyle(fontWeight: FontWeight.w500),
-            ),
-            subtitle: Text(
-              todos[index].description,
-            ),
-            trailing: CircleAvatar(
-              radius: 12,
-              backgroundColor: Colors.indigo[300],
-              child: InkWell(
-                onTap: () {},
-                child: CircleAvatar(
-                  radius: 10,
-                  backgroundColor: Colors.white,
+          itemBuilder: (context, index) => Slidable(
+            startActionPane: todos[index].completed == 0
+                ? ActionPane(motion: const ScrollMotion(), children: [
+                    SlidableAction(
+                      onPressed: (BuildContext ctx) => Navigator.of(context)
+                          .push(MaterialPageRoute(
+                              builder: ((context) => EditView(
+                                  todos[index].title,
+                                  todos[index].description,
+                                  todos[index].id!)))),
+                      backgroundColor: Colors.indigo.shade300,
+                      icon: CupertinoIcons.pen,
+                    ),
+                  ])
+                : null,
+            endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                dismissible: DismissiblePane(
+                  onDismissed: () => notifierTodos.delete(todos[index].id!),
                 ),
+                children: [
+                  SlidableAction(
+                    onPressed: (BuildContext ctx) =>
+                        notifierTodos.delete(todos[index].id!),
+                    backgroundColor: Colors.red,
+                    icon: CupertinoIcons.trash,
+                  ),
+                ]),
+            key: const ValueKey(0),
+            child: ListTile(
+              title: Text(
+                todos[index].title,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    decoration: todos[index].completed == 0
+                        ? TextDecoration.none
+                        : TextDecoration.lineThrough),
               ),
+              subtitle: Text(
+                todos[index].description,
+                style: TextStyle(
+                    decoration: todos[index].completed == 0
+                        ? TextDecoration.none
+                        : TextDecoration.lineThrough),
+              ),
+              trailing: todos[index].completed == 0
+                  ? CircleAvatar(
+                      radius: 12,
+                      backgroundColor: Colors.indigo[300],
+                      child: InkWell(
+                        onTap: todos[index].completed == 0
+                            ? () => ref
+                                .read(todoProvider.notifier)
+                                .toggle(todos[index].id!)
+                            : null,
+                        child: const CircleAvatar(
+                          radius: 10,
+                          backgroundColor: Colors.white,
+                        ),
+                      ),
+                    )
+                  : null,
             ),
           ),
         ),
         error: (err, stack) {
-          logger.e(err);
           return Center(
             child: Text(
               "Error: $err, \n\nstacktrace: $stack",
@@ -55,12 +100,15 @@ class HomeView extends HookConsumerWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo.shade300,
         onPressed: () => Navigator.of(context).push(
           MaterialPageRoute(
-            builder: (context) => AddView(),
+            builder: (context) => const AddView(),
           ),
         ),
       ),
     );
   }
 }
+
+void doNothing(BuildContext context) {}
