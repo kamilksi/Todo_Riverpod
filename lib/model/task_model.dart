a5b088be3232ef637e20712d1fac499e75426a58
kamilksi <kamilksiazek002@gmail.com>
2023-07-04 15:57:48 +0200

    task model

---
+++ lib/model/task_model.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'task_model.g.dart';
+part 'task_model.freezed.dart';
+
+@freezed
+class Task with _$Task {
+  factory Task({
+    required int id,
+    required String title,
+    required String description,
+    required int completed,
+  }) = _Task;
+
+  factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+}
