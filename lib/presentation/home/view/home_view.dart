4ccc9aed136c92f5acddadd7f5d02de6765843d1
kamilksi <kamilksiazek002@gmail.com>
2023-07-04 17:05:56 +0200

    list view builder

--- lib/presentation/home/view/home_view.dart
+++ lib/presentation/home/view/home_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:todo_riverpod/model/task_model.dart';
+import 'package:todo_riverpod/presentation/add/view/add_view.dart';
 import 'package:todo_riverpod/presentation/home/viewmodel/home_model.dart';
 
 class HomeView extends HookConsumerWidget {
-  const HomeView({super.key});
+  HomeView({super.key});
 
+  var logger = Logger();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final asyncTodos = ref.watch(todoProvider);
@@ -16,18 +19,35 @@ class HomeView extends HookConsumerWidget {
         backgroundColor: Colors.indigo[300],
       ),
       body: asyncTodos.when(
-        data: (todos) => ListView(
-          children: [
-            for (final todo in todos)
-              ListTile(
-                title: Text(todo.title),
-              )
-          ],
+        data: (todos) => ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(
+              todos[index].title,
+              style: TextStyle(fontWeight: FontWeight.w500),
+            ),
+            subtitle: Text(
+              todos[index].description,
+            ),
+            trailing: CircleAvatar(
+              radius: 12,
+              backgroundColor: Colors.indigo[300],
+              child: InkWell(
+                onTap: () {},
+                child: CircleAvatar(
+                  radius: 10,
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            ),
+          ),
         ),
         error: (err, stack) {
-          print(err);
-          return Text(
-            "Error: $err",
+          logger.e(err);
+          return Center(
+            child: Text(
+              "Error: $err, \n\nstacktrace: $stack",
+            ),
           );
         },
         loading: () => const Center(
@@ -35,14 +55,11 @@ class HomeView extends HookConsumerWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(todoProvider.notifier).add(
-              Task(
-                id: 12,
-                title: "loading todo",
-                description: "Hello",
-                completed: 1,
-              ),
-            ),
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => AddView(),
+          ),
+        ),
       ),
     );
   }
